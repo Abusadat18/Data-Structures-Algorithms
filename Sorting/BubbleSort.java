857505f086260a5e105ac30ac9b191aa177c3b15
Abu Sadat Ansari <itssadatansari@gmail.com>
2024-08-10 06:46:12 +0530

    Bubble Sort Optimization using a flag

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -5,10 +5,15 @@ import utilities.Utility;
 public class BubbleSort {
     public static void sort(int[] arr) {
         for (int i=arr.length-1 ; i>=1 ; i--){
+            boolean flag = false;
             for (int j=0 ; j<=i-1 ; j++){
                 if (arr[j] > arr[j+1]){
                     Utility.swapInArray(arr,j,j+1);
+                    flag = true;
                 }
+            }
+            if (!flag) {
+                break;
             }
         }
     }
